214bbab7ca9e7a10f7449a32091f1f0930ae8c17
sophie-kang <sophie@tappytoon.com>
2022-02-10 18:39:44 +0900

    Fix importing css module name

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,5 @@
 import type { AppProps } from 'next/app';
-import '@styleses/globals.css';
+import '@styles/globals.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
     return <Component {...pageProps} />;
